0c7518846ca4e443bfc70a9e2d3cdbe7a8d981ff
kong-wep <pakkapol.20100@gmail.com>
2022-10-08 13:15:23 +0700

    Added Block Creation

--- Assets/Scripts/TrayController.cs
+++ Assets/Scripts/TrayController.cs
@@ -4,23 +4,32 @@ using UnityEngine;
 
 public class TrayController : MonoBehaviour
 {
-    
+    static int MAX_BLOCKS = 10;
+
     public GameObject blockPrefab;
+    GameObject[] blocks = new GameObject[MAX_BLOCKS];
+    int block_count = 0;
     // Start is called before the first frame update
     void Start()
     {
-        
+        CreateBlock();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetMouseButtonDown(1)){
+            CreateBlock();
+        }
     }
-    void CreateBlock(){
-        // Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+    int CreateBlock(){ // return index or -1
         // TODO move other blocks here
+        if(block_count >= MAX_BLOCKS)
+            return -1;
         Vector2 pos = new Vector2(0,0);
-        GameObject block = Instantiate(blockPrefab, pos, Quaternion.identity);
+        GameObject block = Instantiate(blockPrefab, pos, Quaternion.identity,transform);
+        blocks[block_count] = block;
+        block_count++;
+        return block_count-1;
     }
 }
