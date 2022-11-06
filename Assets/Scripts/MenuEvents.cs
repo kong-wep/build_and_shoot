5f4ed56f8b8aa833af3efa2e6ca2b1aae6f41f90
BardRoll <thotsapol.nine@gmail.com>
2022-11-06 12:34:58 +0700

    add Menu scene

---
+++ Assets/Scripts/MenuEvents.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuEvents : MonoBehaviour
+{
+    public void LoadLevel(int index)
+    {
+        SceneManager.LoadScene(index);
+    }
+
+    public void Exit()
+    {
+        //Debug.Log("QUIT!");
+        Application.Quit();
+    }
+}
